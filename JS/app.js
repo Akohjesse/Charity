e360795df83c9dde62fd63f1ba27068c7771bd11
JesseFervent <akohgodwinjesse@gmail.com>
2021-01-29 11:32:29 +0100

    Pushing innit files to the repo

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+// animateMenu();
+
+// function animateMenu(){
+//     var menu = document.querySelector('.toggleMenu i');
+//      while (1 > 0){
+//          menu.classList.toggle('.toggleMenuEffect')
+//      }
+// }
