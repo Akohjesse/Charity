136f6f2c2c8364b264bc12617623ade6b8c4890f
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-02-10 19:18:27 +0100

    Updated Changes

--- JS/app.js
+++
@@ -1,9 +0,0 @@
-animateMenu();
-
-function animateMenu(){
-    var menu = document.querySelector('.toggleMenu i');
-     while (1 > 0){
-         menu.classList.toggle('.toggleMenuEffect')
-     }
-}
-
